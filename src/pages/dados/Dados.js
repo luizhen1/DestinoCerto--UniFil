35e335efcdaf0a82d7ddf7f55067926db279892a
luizhen1 <oluizhen1@gmail.com>
2024-09-12 09:12:14 -0300

    first commit

---
+++ src/pages/dados/Dados.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Dados.css'; // Importação do arquivo de estilos CSS
+
+
+const Dados = () => {
+  return (
+    <div className='infos'>
+    <a href="/">
+     <img src="/opa.png"  />
+     </a>
+      <h2>Contato</h2>
+      <p>(43)99999-9999 <br></br>sememail@unifil.com.br</p>
+
+      <h2>Termos</h2>
+      <p>Bem-vindo aos Termos de Serviço do Destino Certo <br></br>Download.</p>
+
+      <h2>Política de Privacidade</h2>
+      <p>Bem-vindo a Política de Privacidade do Destino Certo <br></br>Download.</p>
+
+      <h2>Preços</h2>
+      <p>Whatsapp <br></br> Clique aqui!</p>
+
+      <h2>SAC</h2>
+      <p>Bem-vindo ao SAC do Destino Certo <br></br>Clique aqui!.</p>
+    </div>
+  )
+}
+
+export default Dados
---
+++ src/pages/sobre/Sobre.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Sobre.css'; // Importe o arquivo CSS
+
+const Sobre = () => {
+  return (
+    <div className="container"> {/* Adicione a classe container */}
+    <div>
+    <a href="/">
+        <img src="/opa.png" alt="Destino Certo" />
+    </a>
+    </div>
+      <h1>Sobre Nós</h1>
+      <p>Bem-vindo ao Destino Certo! Somos apaixonados por viagens e queremos 
+        compartilhar essa paixão com você. Navegue por uma variedade de destinos 
+        inspiradores, de clássicos a lugares menos conhecidos. Nosso objetivo é tornar
+        sua próxima viagem inesquecível, fornecendo dicas práticas e sugestões úteis. 
+        Junte-se a nós nesta jornada de descoberta e aventura - o mundo espera por você!</p>
+    </div>
+  );
+}
+
+export default Sobre;
